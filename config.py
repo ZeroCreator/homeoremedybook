64c70d42dbdacdd027f49cb9a88b9fd8d40644e2
Olga Shkola <shkola.olga@gmail.com>
2025-06-09 22:32:42 +0300

    Render

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class Config:
     SECRET_KEY = 'secret'
-    FLATPAGES_ROOT = os.path.join(BASE_DIR, 'homeoremedybook', 'app', 'content')
+    FLATPAGES_ROOT = os.path.join(BASE_DIR, 'src', 'app', 'content')
     FLATPAGES_EXTENSION = '.md'
     FLATPAGES_MARKDOWN_EXTENSIONS = ['fenced_code', 'tables']
-    ACUTE_CASES_FOLDER = os.path.join(BASE_DIR, 'homeoremedybook', 'app', 'content', 'acute_cases')
-    CATEGORY_FOLDER = os.path.join(BASE_DIR, 'homeoremedybook', 'app', 'content', 'categories')
+    ACUTE_CASES_FOLDER = os.path.join(BASE_DIR, 'src', 'app', 'content', 'acute_cases')
+    CATEGORY_FOLDER = os.path.join(BASE_DIR, 'src', 'app', 'content', 'categories')
     UPLOAD_FOLDER = 'static/images'
 
     @staticmethod
